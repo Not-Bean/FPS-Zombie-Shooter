247485c62e71f6b03515f4b39c4ad83302c51fbd
Shallowcat <joshR364@hotmail.com>
2024-11-22 18:55:30 -0500

    this is from months ago i dont know

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -10,7 +10,7 @@ public class Pause : MonoBehaviour
 {
     [SerializeField] GameObject PauseScreen;
     PlayerControler playerControl;
-    bool isPaused = false;
+    public bool isPaused = false;
 
     private void Start()
     {
@@ -22,30 +22,30 @@ public class Pause : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    //public void OnPause()
-    //{
-    //    if (isPaused)
-    //    {
-    //        playerControl.ShootBlock(false); 
-    //        isPaused = false;
-    //        PauseScreen.SetActive(false);
-    //        Time.timeScale = 1;
+    public void OnPause()
+    {
+        if (isPaused)
+        {
+            playerControl.ShootBlock(true);
+            isPaused = false;
+            PauseScreen.SetActive(false);
+            Time.timeScale = 1;
 
-    //        Cursor.lockState = CursorLockMode.Locked;
-    //        Cursor.visible = false;
-    //    }
-    //    else
-    //    {
-    //        playerControl.ShootBlock(true);
-    //        isPaused = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            playerControl.ShootBlock(false);
+            isPaused = true;
 
-    //        PauseScreen.SetActive(true);
-    //        Time.timeScale = 0;
+            PauseScreen.SetActive(true);
+            Time.timeScale = 0;
 
-    //        Cursor.lockState = CursorLockMode.None;
-    //        Cursor.visible = true;
-    //    }
-    //}
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
 
     public void Sensitivity(float value)
     {
