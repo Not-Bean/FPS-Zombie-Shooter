59723e3923dc1d5d9bd5d270a58995facc803943
Omid <145883487+PrinceOfPurge@users.noreply.github.com>
2025-02-28 18:38:48 -0500

    Menu BG-UI
    converted BG to UI-image on cavas

--- Assets/Scripts/SkyChange.cs
+++ Assets/Scripts/SkyChange.cs
@@ -7,18 +7,16 @@ using UnityEngine;
     {
         [SerializeField] private Material skybox;
         private float _elapsedTime = 0f;
-        private float _timeScale = 2.5f; 
+        private float _timeScale = 0.8f; 
         private static readonly int Rotation = Shader.PropertyToID("_Rotation");
         private static readonly int Exposure = Shader.PropertyToID("_Exposure");
 
         void Update()
         {
             _elapsedTime += Time.deltaTime;
-        
-            // Rotate skybox
+            
             skybox.SetFloat(Rotation, _elapsedTime * _timeScale);
-
-            // Slow down the darkening effect
+            
             float exposureValue = Mathf.Clamp(Mathf.Cos(_elapsedTime / 300f), 0.4f, 1f);
             skybox.SetFloat(Exposure, exposureValue);
             
