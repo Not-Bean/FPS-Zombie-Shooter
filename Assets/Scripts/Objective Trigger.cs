2c8509f6b1cd52fb625f7d0117a7d8558c8a6b7d
Not-Bean <alexander.bean@ontariotechu.net>
2025-03-06 15:59:11 -0500

    Working Objectives

---
+++ Assets/Scripts/Objective Trigger.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveTrigger : MonoBehaviour
+{
+    public bool questCompleted;
+
+    public ObjectivesScript os;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        os = GameObject.FindGameObjectWithTag("Player").GetComponent<ObjectivesScript>();
+    }
+
+    void GiveQuest(string quest)
+    {
+        for (int i = 0; i < os.numObjectives; i++)
+        {
+            os.objectiveQuests[i] = quest;
+        }
+
+    }
+
+    public void CompleteQuest()
+    {
+
+        for (int i = 0; i < os.numObjectives; i++)
+        {
+            os.objectiveQuests[i] = "";
+        }
+    
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CompleteQuest();
+            GiveQuest("Leave The Train Station");
+        }
+    }
+}
