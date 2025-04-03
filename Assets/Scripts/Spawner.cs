6607ee6b5d2ed1bf4c28aba85fc9ac4603d7dd1b
Shallowcat <joshR364@hotmail.com>
2025-04-02 20:56:54 -0400

    basic zombie animation fix and spawner changes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,6 +8,8 @@ public class Spawner : MonoBehaviour
     [SerializeField] GameObject zombie;
     [SerializeField] GameObject scoreKeeper;
 
+    GameObject player;
+
     public int startingAmount;
     public int spawnDelay;
     public int maxZombies;
@@ -15,33 +17,49 @@ public class Spawner : MonoBehaviour
 
     int spawnCool;
 
+    bool spawning = true;
+
     List<GameObject> zombies = new List<GameObject> { };
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < startingAmount; i++)
+        if (spawning)
         {
-            GameObject zomb = Instantiate(zombie, new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange)) + transform.position, Quaternion.identity);
-            zomb.GetComponent<Zombie>().SpawnerSet(gameObject);
-            zombies.Add(zomb);
+            for (int i = 0; i < startingAmount; i++)
+            {
+                GameObject zomb = Instantiate(zombie, new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange)) + transform.position, Quaternion.identity);
+                zomb.GetComponent<Zombie>().SpawnerSet(gameObject);
+                zombies.Add(zomb);
+            }
         }
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (zombies.Count < maxZombies && spawnCool <= 0)
+        
+
+        if (zombies.Count < maxZombies && spawnCool <= 0 && spawning)
         {
             GameObject zomb = Instantiate(zombie, new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange)) + transform.position, Quaternion.identity);
             zomb.GetComponent<Zombie>().SpawnerSet(gameObject);
+                
             zombies.Add(zomb);
             spawnCool = spawnDelay;
+
+            if (Vector2.Distance(player.transform.position, zomb.transform.position) < 15)
+            {
+                Destroy(zomb);
+            }
         }
         else
         {
             spawnCool--;
         }
+        
     }
 
     public void dead(GameObject dead)
